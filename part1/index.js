7bfc19c3a75e412d550ea982ef346c7e03e9653a
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-09-02 18:45:45 +0530

    Add initial implementation of add function and update index.js to log values

--- part1/index.js
+++ part1/index.js
@@ -1,1 +1,7 @@
 console.log("hello");
+
+let a  = 10;
+console.log(a);
+
+const data =  new Date()
+console.log(data);
---
+++ part1/localModule1.js
@@ -0,0 +1,9 @@
+import add from "./localModule.js";
+// const add = require("./localModule");
+
+
+const result = add(3,6)
+console.log(result);
+
+const result1 = add(30,60)
+console.log(result1);
