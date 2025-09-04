19183740c87ad2b6554b1bbe26982179ab70c6a7
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-09-04 18:57:12 +0530

    Refactor buffer.js: Comment out console logs and improve buffer handling

--- part1/buffer/buffer.js
+++ part1/buffer/buffer.js
@@ -15,5 +15,30 @@ let arr = new Array(10,20);
 
 // Buffer Length
 let BufLength = buf1.write("welcome")
-console.log(BufLength);
+// console.log(BufLength);
+
+
+
+//Read Buffer 
+readBuffer  = new Buffer(26);
+for (let i = 0; i <= 26; i++) {
+    readBuffer[i] = i + 65 ;
+}
+
+// console.log(readBuffer);
+// console.log(readBuffer.toString());
+// console.log(readBuffer.toString('ascii',4,10));
+// console.log(readBuffer.toString('ascii',0,10));
+//console.log(readBuffer.toString('utf-8',4,10));
+// console.log(readBuffer.toJSON(readBuffer));
+
+
+
+//Concate buffer
+let con1 = new Buffer("Vishal");
+let con2 = new Buffer("Boudhh");
+
+const con3 = Buffer.concat([con1,con2]);
+// console.log(con3);
+// console.log(con3.toString());
 
