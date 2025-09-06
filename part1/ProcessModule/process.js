1a78224e2afb68b318455fa736cf0398de8580e2
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-09-06 18:48:43 +0530

    Add process.js file with commented-out console logs for process information

---
+++ part1/ProcessModule/process.js
@@ -0,0 +1,16 @@
+// Process Module
+
+// console.log("Process env:",process.env);
+// console.log("Process arch:",process.arch);
+// console.log("Process pid:",process.pid);
+// console.log("Process version:",process.version);
+// console.log("Process Argv:",process.argv);
+// console.log("Process Argv:",process.argv[1]);
+// console.log("Process Platform:",process.platform);
+// console.log("process cdw module",process.cwd());
+
+
+// process.argv.forEach((value,index) => {
+//     console.log(`${index}: ${value}`);
+// });
+
