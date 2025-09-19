8c455e5b45b89ac0f159623cd8d04f056a37fcbb
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-09-19 18:56:14 +0530

    Implement HTTP server with routing for home, about, and code pages; update port to 3000

--- NodeJs/HttpModule/http.js
+++ NodeJs/HttpModule/http.js
@@ -1,8 +1,31 @@
 const http = require('http')
-
 // console.log(http);
 
+let PORT = 3000
 http.createServer((req,res)=>{
-        res.write('Hello http module');
-        res.end();
-}).listen(1000)
+    if (req.url === "/") {
+        res.setHeader("Content-Type","text/html")
+        res.write("<h1>Hello Vishal</h1>")
+        res.end(); 
+    }
+    else if(req.url === "/about") {
+        res.setHeader("Content-Type","text/html")
+        res.write("<h1>About Vishal</h1>")
+        res.end(); 
+    }
+    else if(req.url === "/code") {
+        res.setHeader("Content-Type","text/html")
+        res.write("<h1>Code Vishal</h1>")
+        res.end(); 
+    }
+    else{
+        res.setHeader("Content-Type","text/html")
+        res.write("<h1>File dose not exist</h1>")
+        res.end(); 
+    }
+    
+       
+}).listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+})
+
